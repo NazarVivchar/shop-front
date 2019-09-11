85fc2166b670c9f6d9c723d1a9eb6e25cff25678
NazarVivchar <sirstrash.xl@gmail.com>
2019-09-11 22:35:52 +0300

    Updates

---
+++ precache-manifest.27d646835630c739b773f74506b1eb2d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4509c3698f7f9ed59109c549b446e3cd",
+    "url": "/simple-shop/index.html"
+  },
+  {
+    "revision": "0e944e8274d58028d8b9",
+    "url": "/simple-shop/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "38eda3fdabe07a56674e",
+    "url": "/simple-shop/static/js/2.095a5d90.chunk.js"
+  },
+  {
+    "revision": "0e944e8274d58028d8b9",
+    "url": "/simple-shop/static/js/main.3fe312b3.chunk.js"
+  },
+  {
+    "revision": "75b0e1b97d2a1fb93756",
+    "url": "/simple-shop/static/js/runtime~main.fec26f5d.js"
+  }
+]);
