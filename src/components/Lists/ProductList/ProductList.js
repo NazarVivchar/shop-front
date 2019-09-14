2fda198284cf699c9f26417d5efd36bda533415d
NazarVivchar <sirstrash.xl@gmail.com>
2019-09-14 20:56:57 +0300

    Added message informing about lack of products in selected category

--- src/components/Lists/ProductList/ProductList.js
+++ src/components/Lists/ProductList/ProductList.js
@@ -1,21 +1,51 @@
 import ProductCard from "../../Product/ProductCard";
-import React from "react";
+import React, {Component} from "react";
 import Grid from "@material-ui/core/Grid";
+import {Typography} from "@material-ui/core";
+import withTheme from "@material-ui/core/styles/withTheme";
 
-function productList(props) {
-    return (
-        <Grid
-            container
-            alignItems="stretch"
-            justify="flex-start">
-            {props.products.map(product => (
+class productList extends Component {
+    areProductsAvailable() {
+        return this.props.products && this.props.products.length;
+    }
+
+    renderProducts() {
+        return (
+            <Grid
+                container
+                alignItems="stretch"
+                justify="flex-start">
+                {this.props.products.map(product => (
                     <Grid item key={product.id}>
                         <ProductCard product={product}/>
                     </Grid>
-                )
-            )}
-        </Grid>
-    )
+                ))}
+            </Grid>
+        )
+    }
+
+    renderNoAvailableProducts() {
+        return (
+            <Grid
+                container
+                alignItems="center"
+                justify="center">
+                <Typography
+                    variant="h2"
+                    style={{marginTop: this.props.theme.spacing(30)}}>
+                    Товари не знайдено
+                </Typography>
+            </Grid>
+        )
+    }
+
+    render() {
+        return (
+            this.areProductsAvailable()
+                ? this.renderProducts()
+                : this.renderNoAvailableProducts()
+        )
+    }
 }
 
-export default productList;
+export default withTheme(productList);
