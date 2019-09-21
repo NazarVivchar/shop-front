2cb858567a749d19556b4f01574521902fcb21f0
NazarVivchar <sirstrash.xl@gmail.com>
2019-09-21 16:38:12 +0300

    Added validation for category form

--- src/components/dialogs/CategoryDialog/CategoryDialog.js
+++ src/components/dialogs/CategoryDialog/CategoryDialog.js
@@ -2,90 +2,117 @@ import Dialog from "@material-ui/core/Dialog";
 import React, {Component} from "react";
 import DialogTitle from "@material-ui/core/DialogTitle";
 import DialogContent from "@material-ui/core/DialogContent";
-import {Formik} from 'formik';
-import TextField from "@material-ui/core/TextField";
+import {Field, Form, Formik} from 'formik';
 import Grid from "@material-ui/core/Grid";
 import DialogActions from "@material-ui/core/DialogActions";
 import Button from "@material-ui/core/Button";
 import withTheme from "@material-ui/core/styles/withTheme";
+import "./CategoryDialog.scss";
+import * as Yup from "yup";
+import TextField from "@material-ui/core/TextField";
+import Typography from "@material-ui/core/Typography";
 
 class CategoryDialog extends Component {
     state = {
         ...this.props.initialState
     };
 
-    resetState() {
-        this.setState({
-            ...this.props.initialState
-        })
-    }
-
     componentDidMount() {
         this.props.loadData();
     }
 
-    handleChange = event => {
-        const target = event.target;
-        this.setState({
-            category: {
-                ...this.state.category,
-                [target.name]: target.value
-            }
-        })
-    };
+    renderTextField(type, label, autoComplete) {
+        return function ({field, name, value, onChange, onBlur}) {
+            return (
+                <TextField
+                    {...field}
+                    {...name}
+                    {...value}
+                    {...onBlur}
+                    {...onChange}
+                    autoComplete={autoComplete}
+                    type={type}
+                    label={label}
+                />
+            )
+        }
+    }
 
-    handleSubmit = () => {
-        const category = this.state.category;
-        this.props.handleSubmit(category);
-        this.props.handleClose();
-    };
-    
-    handleClose = () => {
-        this.props.handleClose();
-        this.resetState();
-    };
+    renderErrorMessage(errors, touched, name) {
+        return (
+            <Typography
+                color="error">
+                {errors[name] && touched[name] && errors[name]}
+                &nbsp;
+            </Typography>
+        )
+    }
 
     render() {
         const {theme} = this.props;
+        const ValidationSchema = Yup.object().shape({
+            name: Yup.string()
+                .max(50, "Назва не можу бути довшою 50 символів")
+                .required('Це поле обов\'язкове'),
+        });
 
         return (
             <Dialog
                 open={this.props.isOpened}
-                onClose={this.handleClose}
+                onClose={this.props.handleClose}
                 PaperProps={{style: {margin: theme.spacing(2)}}}>
                 <DialogTitle>
                     Товар
                 </DialogTitle>
                 <DialogContent>
                     <Formik
-                        render={props => (
-                            <Grid container
-                                  direction="column"
-                                  alignItems={"stretch"}
-                                  justify={"space-between"}
-                                  className="dialog">
-                                <TextField
-                                    type="text"
-                                    onChange={this.handleChange}
-                                    value={this.state.category.name}
-                                    name="name"
-                                    label="Назва"
-                                />
-                            </Grid>
+                        initialValues={
+                            this.props.category
+                                ? {
+                                    name: this.props.category.name,
+                                }
+                                : {
+                                    name: "",
+                                }
+                        }
+                        validationSchema={ValidationSchema}
+                        onSubmit={values => {
+                            const category = {
+                                ...values,
+                                id: this.state.category.id,
+                            };
+                            this.props.handleSubmit(category);
+                            this.props.handleClose();
+                        }}
+                    >
+                        {({errors, touched}) => (
+                            <Form
+                                context={{isRegisterForm: this.props.isRegisterForm}}>
+                                <Grid container
+                                      direction="column"
+                                      alignItems={"stretch"}
+                                      justify="space-around"
+                                      className="dialog"
+                                      style={{height: "100px"}}>
+                                    <Field name="name"
+                                           render={this.renderTextField("text", "Назва")}/>
+                                    {this.renderErrorMessage(errors, touched, "name")}
+                                </Grid>
+                                <DialogActions>
+                                    <Button
+                                        onClick={this.props.handleClose}>
+                                        Скасувати
+                                    </Button>
+                                    <Button
+                                        type="submit"
+                                        color="secondary">
+                                        Зберегти
+                                    </Button>
+                                </DialogActions>
+                            </Form>
                         )}
-                    />
+                    </Formik>
                 </DialogContent>
-                <DialogActions>
-                    <Button
-                        onClick={this.handleClose}>
-                        Скасувати
-                    </Button>
-                    <Button
-                        color="secondary"
-                        onClick={this.handleSubmit}>
-                        Зберегти
-                    </Button>
-                </DialogActions>
             </Dialog>
         )
     }
